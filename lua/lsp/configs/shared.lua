2b9c0d52ab49ac5014aa9339178d1ef45cc9c646
Heiker Curiel <heikercuriel@gmail.com>
2022-05-18 09:41:27 -0400

    refactor(configs): move diagnostics and handlers to init.lua

--- lua/lsp/configs/shared.lua
+++ lua/lsp/configs/shared.lua
@@ -7,6 +7,25 @@ local fmt = string.format
 
 local server_group = 'LSP_server_%s'
 
+M.make_config = function(config)
+  local defaults = {
+    root_dir = vim.fn.getcwd(),
+    capabilities = M.capabilities,
+    on_attach = M.on_attach,
+    on_init = M.on_init,
+    on_exit = M.on_exit,
+    flags = {
+      debounce_text_changes = 150,
+    },
+  }
+
+  return vim.tbl_deep_extend(
+    'force',
+    defaults,
+    config
+  )
+end
+
 M.on_init = function(client, results)
   if results.offsetEncoding then
     client.offset_encoding = results.offsetEncoding
@@ -52,89 +71,6 @@ M.on_attach = function(client, bufnr)
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
-
-M.make_config = function(config)
-  local defaults = {
-    root_dir = vim.fn.getcwd(),
-    capabilities = M.capabilities,
-    on_attach = M.on_attach,
-    on_init = M.on_init,
-    on_exit = M.on_exit,
-    flags = {
-      debounce_text_changes = 150,
-    },
-  }
-
-  return vim.tbl_deep_extend(
-    'force',
-    defaults,
-    config
-  )
-end
-
-M.diagnostics = function()
-  local sign = function(opts)
-    vim.fn.sign_define(opts.name, {
-      texthl = opts.name,
-      text = opts.text,
-      numhl = ''
-    })
-  end
-
-  sign({name = 'DiagnosticSignError', text = '✘'})
-  sign({name = 'DiagnosticSignWarn', text = '▲'})
-  sign({name = 'DiagnosticSignHint', text = '⚑'})
-  sign({name = 'DiagnosticSignInfo', text = ''})
-
-  vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = true,
-    float = {
-      focusable = false,
-      style = 'minimal',
-      border = 'rounded',
-      source = 'always',
-      header = '',
-      prefix = '',
-    },
-  })
-
-  local group = augroup('diagnostics_cmds', {clear = true})
-
-  autocmd('ModeChanged', {
-    group = group,
-    pattern = {'n:i', 'v:s'},
-    desc = 'Disable diagnostics while typing',
-    callback = function() vim.diagnostic.disable(0) end
-  })
-
-  autocmd('ModeChanged', {
-    group = group,
-    pattern = 'i:n',
-    desc = 'Enable diagnostics when leaving insert mode',
-    callback = function() vim.diagnostic.enable(0) end
-  })
-end
-
-M.handlers = function()
-  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-    vim.lsp.handlers.hover,
-    {border = 'rounded'}
-  )
-
-  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-    vim.lsp.handlers.signature_help,
-    {border = 'rounded'}
-  )
-end
-
-M.setup = function()
-  M.handlers()
-  M.diagnostics()
-end
 
 return M
 
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,8 +1,68 @@
 local M = {}
 
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
 local lsp = require('lsp.client')
 
-require('lsp.configs.shared').setup()
+M.diagnostics = function()
+  local sign = function(opts)
+    vim.fn.sign_define(opts.name, {
+      texthl = opts.name,
+      text = opts.text,
+      numhl = ''
+    })
+  end
+
+  sign({name = 'DiagnosticSignError', text = '✘'})
+  sign({name = 'DiagnosticSignWarn', text = '▲'})
+  sign({name = 'DiagnosticSignHint', text = '⚑'})
+  sign({name = 'DiagnosticSignInfo', text = ''})
+
+  vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = 'minimal',
+      border = 'rounded',
+      source = 'always',
+      header = '',
+      prefix = '',
+    },
+  })
+
+  local group = augroup('diagnostics_cmds', {clear = true})
+
+  autocmd('ModeChanged', {
+    group = group,
+    pattern = {'n:i', 'v:s'},
+    desc = 'Disable diagnostics while typing',
+    callback = function() vim.diagnostic.disable(0) end
+  })
+
+  autocmd('ModeChanged', {
+    group = group,
+    pattern = 'i:n',
+    desc = 'Enable diagnostics when leaving insert mode',
+    callback = function() vim.diagnostic.enable(0) end
+  })
+end
+
+M.handlers = function()
+  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+    vim.lsp.handlers.hover,
+    {border = 'rounded'}
+  )
+
+  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+    vim.lsp.handlers.signature_help,
+    {border = 'rounded'}
+  )
+end
 
 M.project_setup = function(list)
   for _, server in pairs(list) do
@@ -11,6 +71,9 @@ M.project_setup = function(list)
 end
 
 M.start = lsp.start
+
+M.diagnostics()
+M.handlers()
 
 return M
 
