742e205d1dafaa1bcae28f558d2fbf4749d22709
Heiker Curiel <heikercuriel@gmail.com>
2022-10-16 22:00:38 -0400

    feat: call ex-commmands as functions

--- lua/user/sessions.lua
+++ lua/user/sessions.lua
@@ -1,20 +1,19 @@
 local autocmd = vim.api.nvim_create_autocmd
 local command = vim.api.nvim_create_user_command
-local escape = vim.fn.fnameescape
-local fmt = string.format
-
 local session_path = './.nvim/Session.vim'
 
 local save_session = function()
   local session = vim.v.this_session
-  if session == '' then return end
+  if session == '' then
+    return
+  end
 
-  vim.cmd(fmt('mksession! %s', escape(session)))
+  vim.cmd.mksession({session, bang = true})
 end
 
 local load_session = function(input)
   save_session()
-  vim.cmd(fmt('source %s', escape(session_path)))
+  vim.cmd.source(session_path)
 end
 
 command('SessionLoad', load_session, {})
@@ -22,21 +21,25 @@ command('SessionSave', save_session, {})
 
 command('SessionNew', function(input)
   local session = vim.v.this_session
-  if session ~= '' then return end
+  if session ~= '' then
+    return
+  end
 
   if vim.fn.filereadable(session_path) == 0 then
     vim.fn.mkdir(vim.fn.fnamemodify(session_path, ':h'), 'p')
   end
 
-  vim.cmd(fmt('mksession %s', escape(session_path)))
+  vim.cmd.mksession(session_path)
 end, {})
 
 command('SessionConfig', function()
   local session = vim.v.this_session
-  if session == '' then return end
+  if session == '' then
+    return
+  end
 
   local path = vim.fn.fnamemodify(session, ':r')
-  vim.cmd(fmt('edit %sx.vim', escape(path)))
+  vim.cmd.edit(path .. 'x.vim')
 end, {})
 
 autocmd('VimLeavePre', {
